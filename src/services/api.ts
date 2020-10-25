fedd9d20cbf3233dbbb0e5574085c76aecd0f373
yonlu <lss.emailbox@gmail.com>
2020-10-25 01:05:20 -0400

    feat: connected to happy-api on heroku

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://75.102.79.122:3333",
+  baseURL: "https://happy-typescript-api.herokuapp.com",
 });
 
 export default api;
